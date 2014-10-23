ff05cabb7455b80cf0497d0e5c04359851c97b0f
LiesBollens <lies.bollens@gmail.com>
2014-10-23 17:59:48 +0200

    website aanpassingen

---
+++ main_website/js/main.js
@@ -0,0 +1,21 @@
+/**
+ * Created by liesbollens on 23/10/14.
+ */
+
+/*
+
+
+dataController = (function(){
+
+    function init() {
+
+        $("#bp-icon-about").click(function () {
+            $('#detailSection').css("display","none");
+            });
+
+    }
+
+    return{
+        init:init;
+    },
+});*/
