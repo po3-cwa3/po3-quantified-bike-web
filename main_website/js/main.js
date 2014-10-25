87b73eeef5a379b4d84e5d946931ed16f5c82491
LiesBollens <lies.bollens@gmail.com>
2014-10-25 10:34:46 +0200

    about-us en about-device added

--- main_website/js/main.js
+++ main_website/js/main.js
@@ -1,21 +1,51 @@
-/**
- * Created by liesbollens on 23/10/14.
- */
-
-/*
 
 
-dataController = (function(){
+dataController = ( function () {
+
 
     function init() {
+        var counterus = 0;
+        var counterdevice = 0;
 
-        $("#bp-icon-about").click(function () {
-            $('#detailSection').css("display","none");
-            });
+        $("#about").click(function () {
+            if (counterus){
+                $('#about-us').css("display", "none");
+                counterus = 0;
+            }
+            else {
+                $('#about-device').css("display", "none");
+                $('#about-us').css("display", "block");
+                counterus = 1;
+            }
+        });
 
+        $("#detailCloseButton").click(function () {
+            $('#about-us').css("display", "none");
+        });
+
+        $("#device").click(function () {
+            if (counterdevice){
+                $('#about-device').css("display", "none");
+                counterdevice = 0;
+            }
+            else {
+                $('#about-us').css("display", "none");
+                $('#about-device').css("display", "block");
+                counterdevice = 1;
+            }
+        });
+
+        $("#detailCloseButton2").click(function () {
+            $('#about-device').css("display", "none");
+        });
     }
 
-    return{
-        init:init;
-    },
-});*/
+    return {
+        init: init
+     };
+
+
+})();
+
+$(document).ready(dataController.init);
+
