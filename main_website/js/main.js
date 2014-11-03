4d60cb0d9477615f4c89ce41a49ad31ed40d5e69
LiesBollens <lies.bollens@gmail.com>
2014-11-03 16:40:32 +0100

    merge conflict

--- main_website/js/main.js
+++ main_website/js/main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(document).ready(function() {
 
     var counterus = 0;
@@ -45,58 +45,5 @@ $(document).ready(function() {
     });
 
 });
-=======
 
 
-dataController = ( function () {
-
-
-    function init() {
-        var counterus = 0;
-        var counterdevice = 0;
-
-        $("#about").click(function () {
-            if (counterus){
-                $('#about-us').css("display", "none");
-                counterus = 0;
-            }
-            else {
-                $('#about-device').css("display", "none");
-                $('#about-us').css("display", "block");
-                counterus = 1;
-            }
-        });
-
-        $("#detailCloseButton").click(function () {
-            $('#about-us').css("display", "none");
-        });
-
-        $("#device").click(function () {
-            if (counterdevice){
-                $('#about-device').css("display", "none");
-                counterdevice = 0;
-            }
-            else {
-                $('#about-us').css("display", "none");
-                $('#about-device').css("display", "block");
-                counterdevice = 1;
-            }
-        });
-
-        $("#detailCloseButton2").click(function () {
-            $('#about-device').css("display", "none");
-        });
-
-
-    }
-
-    return {
-        init: init
-     };
-
-
-})();
-
-$(document).ready(dataController.init);
-
->>>>>>> a1dc873dffb1b38748f904f2c11b71e193abd33d
